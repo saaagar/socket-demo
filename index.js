b1e7cce2fc22c30384184a7c3d2d6f153ac67019
saaagar <sagar.mishra2048@gmail.com>
2022-03-20 19:45:02 +1000

    Changed port number to 80

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, ()=> {
-    console.log('listening on *:3000');
+server.listen(80, ()=> {
+    console.log('listening on *:80');
 });
