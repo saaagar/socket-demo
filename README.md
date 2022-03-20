b62f1f4c832d74088b2e59cf94f8c0e116e03c4d
saaagar <sagar.mishra2048@gmail.com>
2022-03-20 19:35:39 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Simple chat app
+This app uses socket.io for dispatching and subscribing events.
+
+## Features
+* Joining chat room with a username
+* Notifies when user joins or leaves the chat room
+
+## Running locally
+Clone this repo on your machine 
+
+```$ git clone https://github.com/saaagar/socket-demo.git``` 
+
+cd to the freshly cloned dir and run 
+
+```$ node index.js```  
+
+Server listens to port 3000, but you can change to any port number that is available on your machine.
+
+
+
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+var users = {};
+
+io.on('connection', (socket) => {
+
+    socket.on('join', function(username) {
+        users[socket.id] = username;
+        socket.emit('update',`Hi 👋  ${username}, welcome to this simple chat app.`);
+        io.emit('update', `${username} has joined the chat.`);
+        io.emit('update-users',users);
+    });
+
+    socket.on('chat message',(msg) => {
+        io.emit('chat message', msg, users[socket.id]);
+    });
+
+    socket.on('typing',() => {
+        io.emit('typing',`${users[socket.id]} is typing a message ...`);
+    })
+
+    socket.on('disconnect',function(){
+        io.emit('update',`${users[socket.id]} has left the chat.`);
+        delete users[socket.id];
+        io.emit('update-users', users);
+    });
+});
+
+server.listen(3000, ()=> {
+    console.log('listening on *:3000');
+});
